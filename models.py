6888397d47fe975967c6fd974bcc0d4aaf6447ab
Edeko Eromosele <eromoseleedeko@gmail.com>
2024-06-16 22:48:08 +0100

    flip for card added

--- models.py
+++ models.py
@@ -2,3 +2,13 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+class Flashcard(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    table_name = db.Column(db.String(150), nullable=False)
+    question = db.Column(db.Text, nullable=False)
+    explanation = db.Column(db.Text, nullable=False)
+
+class Favorite(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    flashcard_id = db.Column(db.Integer, nullable=False)
